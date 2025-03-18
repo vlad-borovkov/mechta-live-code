082a84429f706b20e280c83afbfd470ac43dd462
vlad_borovkov <borovkov-vlad@ya.ru>
2025-03-18 10:24:37 +0500

    init-commit

---
+++ src/tasks/Compare.vue
@@ -0,0 +1,29 @@
+<script setup lang="js">
+import { MICard, MITag } from "mechtaui";
+// что вернут эти сравнения?
+function checkComparisons() {
+  console.log("1)", 0 == "0"); // ?
+  console.log("2)", 0 === "0"); // ?
+  console.log("3)", "" == false); // ?
+  console.log("4)", "" === false); // ?
+  console.log("5)", 0 || "0"); // ?
+  console.log("6)", 0 && "0"); // ?
+  console.log("7)", false || ""); // ?
+  console.log("8)", false && ""); // ?
+  console.log("9)", null == undefined); // ?
+  console.log("10)", null === undefined); // ?
+}
+
+// checkComparisons();
+</script>
+
+<template>
+  <div class="flex flex-col gap-4">
+    <MITag tag-value="/tasks/Compare.vue" type="info"></MITag>
+    <MICard>
+      <h2 class="font-semibold mb-2">Compare</h2>
+      <p>Что вернут в консоль эти сравнения?<br /> Ответы оставляй напротив каждого. <br /> По завершению, запустим
+        <MITag type="outline" tag-value="checkComparisons()"></MITag></p>
+    </MICard>
+  </div>
+</template>
---
+++ src/tasks/FunctionalChain.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import { MICard, MITag } from "mechtaui";
+/*******************************************
+
+ЗАДАЧА: написать конфигурируемую цепочку вызовов функций,
+чтобы в консоль выводилась отформатированная марка авто.
+Пример интерфейса и вызова
+
+getCar(конфигурация регистра: нижний | верхний | undefined).print('марка авто', 'модель')
+getCar(toUpper).print('Honda', 'Fit') --> HONDA FIT
+
+
+Модульный стиль
+Фабричный метод
+Функциональный стиль 
+Использует замыкания 
+
+/******************************************* */
+type CaseTransform = "UpperCase" | "LowerCase" | undefined;
+
+const getCar = (caseTransform: CaseTransform) => {
+  return {
+    print: (brand: string, model: string) => {
+      let formatted = `${brand} ${model}`;
+      if (caseTransform === "LowerCase") {
+        formatted = formatted.toLowerCase();
+      } else if (caseTransform === "UpperCase") {
+        formatted = formatted.toUpperCase();
+      }
+      console.log(formatted);
+    },
+  };
+};
+
+getCar("UpperCase").print("Toyota", "Camry");
+getCar("LowerCase").print("Toyota", "Corolla");
+getCar(undefined).print("Mercedes-benz", "E 350");
+
+</script>
+
+<template>
+    <div class="flex flex-col gap-4">
+        <MITag tag-value="./tasks/FunctionalChain.vue" type="info"></MITag>
+        <MICard class="flex flex-col gap-4">
+            <h2 class="font-semibold mb-2">FunctionalChain</h2>
+            <code>
+                ЗАДАЧА: написать конфигурируемую цепочку вызовов функций,
+                чтобы в консоль выводилась отформатированная марка авто.
+            </code>
+
+            <p>Пример интерфейса и вызова:</p>
+
+            <code>
+            getCar(конфигурация регистра: нижний | верхний | undefined).print('марка авто', 'модель')
+            getCar(toUpper).print('Honda', 'Fit') -> HONDA FIT
+            </code>
+
+            <p>Будет полезным придерживаться даннных принципов:</p>
+            <ul>
+                <li>Модульный стиль (инкапсуляция вспомогательных функций)</li>
+                <li> Фабричный метод </li>
+                <li> Функциональный стиль (чистые функции, отсутствие мутаций)</li>
+                <li> Использует замыкания</li>
+            </ul>
+        </MICard>
+    </div>
+</template>
