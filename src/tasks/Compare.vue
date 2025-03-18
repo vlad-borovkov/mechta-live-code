6b99cab560ae99159cfe1c49765b64b6b2e3fcb1
vlad-borovkov <borovkov-vlad@ya.ru>
2025-03-18 13:20:07 +0500

    feat(Compare): new-layout-and-logic

--- src/tasks/Compare.vue
+++ src/tasks/Compare.vue
@@ -1,29 +1,70 @@
 <script setup lang="js">
-import { MICard, MITag } from "mechtaui";
+import { MIButton, MICard, MIInput, MITag } from 'mechtaui';
+import { computed, reactive, ref } from 'vue';
 // что вернут эти сравнения?
-function checkComparisons() {
-  console.log("1)", 0 == "0"); // ?
-  console.log("2)", 0 === "0"); // ?
-  console.log("3)", "" == false); // ?
-  console.log("4)", "" === false); // ?
-  console.log("5)", 0 || "0"); // ?
-  console.log("6)", 0 && "0"); // ?
-  console.log("7)", false || ""); // ?
-  console.log("8)", false && ""); // ?
-  console.log("9)", null == undefined); // ?
-  console.log("10)", null === undefined); // ?
-}
+const comparesArr = reactive([
+  { compare: 'console.log(0 == "0")', value: '', execute: () => 0 == '0' },
+  { compare: 'console.log(0 === "0")', value: '', execute: () => 0 === '0' },
+  { compare: 'console.log("" == false)', value: '', execute: () => '' == false },
+  { compare: 'console.log("" === false)', value: '', execute: () => '' === false },
+  { compare: 'console.log(0 || "0")', value: '', execute: () => 0 || '0' },
+  { compare: 'console.log(0 && "0")', value: '', execute: () => 0 && '0' },
+  { compare: 'console.log(false || "")', value: '', execute: () => false || '' },
+  { compare: 'console.log(false && "")', value: '', execute: () => false && '' },
+  { compare: 'console.log(undefined == null)', value: '', execute: () => undefined == null },
+  { compare: 'console.log(undefined === null)', value: '', execute: () => undefined === null },
+]);
 
+const isValidAnswer = computed(() => {
+  return comparesArr.every(item => item.value.length > 0);
+});
+const isShowRightAnswer = ref(false);
+
+function handleCallFunc(func, isShow) {
+  if (typeof func === 'function' && isShow)
+    return func();
+}
 // checkComparisons();
 </script>
 
 <template>
   <div class="flex flex-col gap-4">
-    <MITag tag-value="/tasks/Compare.vue" type="info"></MITag>
+    <MITag tag-value="/tasks/Compare.vue" type="info" />
     <MICard>
-      <h2 class="font-semibold mb-2">Compare</h2>
-      <p>Что вернут в консоль эти сравнения?<br /> Ответы оставляй напротив каждого. <br /> По завершению, запустим
-        <MITag type="outline" tag-value="checkComparisons()"></MITag></p>
+      <h2 class="font-semibold mb-2">
+        Compare
+      </h2>
+      <p>
+        Что вернут эти сравнения? Ответы оставляй напротив каждого. По завершению, запустим код.
+        <MITag type="outline" tag-value="checkComparisons()" />
+      </p>
+
+      <ol class="flex flex-col gap-4 mt-4">
+        <li
+          v-for="(item, index) in comparesArr"
+          :key="item.compare"
+          class="flex gap-2 w-1/2"
+        >
+          <span>{{ `${index + 1})` }}</span>
+          <p class="font-bold text-md">
+            {{ item.compare }}
+          </p>
+          <MIInput
+            v-model="item.value"
+            placeholder="Ответ"
+            size="MD"
+            wrapper-class="w-[20%]"
+          />;
+          <p>{{ handleCallFunc(item?.execute, isShowRightAnswer) }}</p>
+          <p>{{ isShowRightAnswer ? typeof handleCallFunc(item?.execute, isShowRightAnswer) : '' }}</p>
+        </li>
+      </ol>
+      <MIButton
+        :disabled="!isValidAnswer"
+        @click="isShowRightAnswer = !isShowRightAnswer"
+      >
+        Узнать ответы
+      </MIButton>
     </MICard>
   </div>
 </template>
